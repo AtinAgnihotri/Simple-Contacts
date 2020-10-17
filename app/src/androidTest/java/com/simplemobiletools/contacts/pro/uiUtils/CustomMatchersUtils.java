6d3eb78db570ae92c466e45e7b7cf236608e4223
Atin Agnihotri <atinagnihotri@gmail.com>
2020-10-18 03:41:28 +0530

    Add Custom nthChildOf Matcher

--- app/src/androidTest/java/com/simplemobiletools/contacts/pro/uiUtils/CustomMatchersUtils.java
+++ app/src/androidTest/java/com/simplemobiletools/contacts/pro/uiUtils/CustomMatchersUtils.java
@@ -36,5 +36,26 @@ public class CustomMatchersUtils {
         };
     }
 
-    
+    @NotNull
+    public static Matcher<View> nthChildOf(final Matcher<View> parentMatcher, final int childPosition) {
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("position " + childPosition + " of parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                if (!(view.getParent() instanceof ViewGroup)) return false;
+                ViewGroup parent = (ViewGroup) view.getParent();
+
+                return parentMatcher.matches(parent)
+                        && parent.getChildCount() > childPosition
+                        && parent.getChildAt(childPosition).equals(view);
+            }
+        };
+    }
+
+
 }
