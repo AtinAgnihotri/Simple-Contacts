be03453a03cdea1b03a5097b24ad4dbaeb646843
Atin Agnihotri <atinagnihotri@gmail.com>
2020-10-18 04:04:07 +0530

    Adding AddContactToContactsList E2E Test

---
+++ app/src/androidTest/java/com/simplemobiletools/contacts/pro/TestContactInsertion.java
@@ -0,0 +1,24 @@
+package com.simplemobiletools.contacts.pro;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.simplemobiletools.contacts.pro.testWorkflows.AddContactWorkflow;
+import com.simplemobiletools.contacts.pro.testWorkflows.ContactValidationWorkflow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ *
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class TestContactInsertion extends BaseTestCase{
+
+    @Test
+    public void test_E2E_AddContactToContactsList() {
+        AddContactWorkflow.whenTestContactAddedOnContactsList();
+        ContactValidationWorkflow.thenTestContactExistsInContactList();
+    }
+
+}
