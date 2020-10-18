2d2287270aba6baa85764ff86e174a3cd44e0cf1
Atin Agnihotri <atinagnihotri@gmail.com>
2020-10-18 13:01:16 +0530

    Add ContactGroupingInContactsList E2E Test

---
+++ app/src/androidTest/java/com/simplemobiletools/contacts/pro/TestContactGroups.java
@@ -0,0 +1,21 @@
+package com.simplemobiletools.contacts.pro;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.simplemobiletools.contacts.pro.testWorkflows.AddContactWorkflow;
+import com.simplemobiletools.contacts.pro.testWorkflows.GroupsWorkflow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class TestContactGroups extends BaseTestCase{
+
+    @Test
+    public void test_E2E_ContactGroupingInContactsList() {
+        AddContactWorkflow.givenMultipleTestContactAddedOnContactsList(false);
+        GroupsWorkflow.whenFirstThreeContactsAreSelectedAndGrouped();
+        GroupsWorkflow.thenTestGroupHasFirstThreeContacts();
+    }
+
+}
